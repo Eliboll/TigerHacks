48ad4e8f37db83557c57db1c38da913c46debc4e
EliBoll <40479800+Eliboll@users.noreply.github.com>
2021-11-04 16:04:06 -0500

    Delete testrunner.java

--- testrunner.java
+++
@@ -1,7 +0,0 @@
-public class testrunner {
-    public static void main(String[] args) {
-        test testrunner = new test();
-        System.out.println(testrunner);
-        System.out.println("Testing");
-    }
-}
